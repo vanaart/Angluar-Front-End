82111fd8615470d9b07e7fcefc85f583fb729ec6
Thomas van Aart <Thomas van Aart>
2017-11-23 13:24:21 +0100

    First Commit

---
+++ Front-End/src/app/recipes/recipe.service.ts
@@ -0,0 +1,51 @@
+import {ChangeDetectorRef, Injectable, Input} from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  public recipes: Recipe[] = [];
+
+  constructor(private slService: ShoppingListService, private http: HttpClient) {}
+
+  getRecipes(): Promise<any[]> {
+    return this.http.get('http://localhost:3000/recipies').toPromise().then(response => {
+      this.recipes = response as Recipe[];
+      return this.recipes;
+    });
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+
+    this.http.post('http://localhost:3000/recipies', recipe).subscribe();
+
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
---
+++ Front-End/src/app/recipes/recipes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+  result: object;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+}
