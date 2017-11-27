8019d680838a2ff245d99ee2a247632b9cbbd3b0
Sem Rekkers <rekkers.sem@gmail.com>
2017-11-27 20:45:12 +0100

    Update URLs

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+//import { DataService } from './shared/data.service';
 
 @NgModule({
   declarations: [
---
+++ Front-End/src/app/shared/data.service.ts
@@ -0,0 +1,9 @@
+export const apiEndpoint = 'https://avans-i23ivt2d-api.herokuapp.com';
+
+// export class DataService {
+//   apiEndpoint: string,
+//
+//   constructor(){
+//     this.apiEndpoint = 'https://avans-i23ivt2d-api.herokuapp.com';
+//   }
+// }
